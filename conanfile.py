830303092b8dbdb09942967ccabfdb7a11d1709e
cppbitman <daihongjun@kedacom.com>
2018-11-30 00:33:56 -0800

    linux build done without gtk-doc

--- conanfile.py
+++ conanfile.py
@@ -19,11 +19,7 @@ class GstreamerConan(ConanFile):
     }
     default_options = { 'shared': False, 'fPIC': True }
     _source_subfolder = "source_subfolder"
-    _build_subfolder = "build_subfolder"
-
-
-    #requires = "libffi/3.3-rc0@conanos/dev","glib/2.58.0@conanos/dev","gobject-introspection/1.58.0@conanos/dev" #gtk-doc-lite
-    
+    _build_subfolder = "build_subfolder"    
 
     def requirements(self):
         self.requires.add("glib/2.58.1@conanos/stable")
@@ -55,7 +51,7 @@ class GstreamerConan(ConanFile):
             self.run('git submodule update --init --recursive')
         if self.settings.os == "Windows":
             tools.patch(patch_file=self.patch)
-        os.rename(extracted_dir, self._source_subfolder)    
+        os.rename(extracted_dir, self._source_subfolder)
 
         #url_ = 'https://github.com/GStreamer/gstreamer/archive/{version}.tar.gz'.format(version=self.version)
         #tools.get(url_)
@@ -66,33 +62,28 @@ class GstreamerConan(ConanFile):
 
 
     def build(self):
-        #with tools.environment_append({"LD_LIBRARY_PATH":'%s/lib'%(self.deps_cpp_info["libffi"].rootpath),
-        #    'PATH':'%s/bin:%s'%(self.deps_cpp_info["gobject-introspection"].rootpath, os.getenv("PATH"))}):
-        #    with tools.chdir(self._source_subfolder):
-        #        meson = Meson(self)
-        #        _defs = { 'prefix':'%s/builddir/install'%(os.getcwd()), 'libdir':'lib',
-        #                  'build_tools':'true', 'disable_gtkdoc':'true', 'library_format': 'shared',
-        #                  'disable_introspection' : 'false', 'disable_libunwind' : 'true',
-        #                }
-        #        meson.configure(
-        #            defs=_defs,
-        #            source_dir = '%s'%(os.getcwd()),
-        #            build_dir= '%s/builddir'%(os.getcwd()),
-        #            pkg_config_paths=['%s/lib/pkgconfig'%(self.deps_cpp_info["libffi"].rootpath),
-        #                              '%s/lib/pkgconfig'%(self.deps_cpp_info["glib"].rootpath),]
-        #            )
-        #        meson.build(args=['-j2'])
-        #        self.run('ninja -C {0} install'.format(meson.build_dir))
         pkg_config_paths=[ os.path.join(self.deps_cpp_info[i].rootpath, "lib", "pkgconfig") for i in ["glib","gtk-doc-lite","libffi","zlib"] ]
         binpath=[ os.path.join(self.deps_cpp_info[i].rootpath, "bin") for i in ["glib","gtk-doc-lite","libffi","zlib"] ]
         prefix = os.path.join(self.build_folder, self._build_subfolder, "install")
+        libpath = [ os.path.join(self.deps_cpp_info[i].rootpath, "lib") for i in ["libffi"] ]
         meson = Meson(self)
         defs = {'prefix' : prefix, 'disable_libunwind':'true','disable_introspection':'true'}
         defs['library_format'] = 'shared' if self.options.shared else 'static'
         if self.settings.os == "Linux":
             defs.update({'libdir':'lib'})
-        with tools.environment_append({
+        if self.settings.os == 'Windows':
+            with tools.environment_append({
                 'PATH' : os.pathsep.join(binpath + [os.getenv('PATH')])
+                }):
+                meson.configure(defs=defs,source_dir=self._source_subfolder, build_dir=self._build_subfolder,
+                                pkg_config_paths=pkg_config_paths)
+                meson.build()
+                self.run('ninja -C {0} install'.format(meson.build_dir))
+        if self.settings.os == "Linux":
+            defs.update({'disable_gtkdoc':'true'})
+            with tools.environment_append({
+                'PATH' : os.pathsep.join(binpath + [os.getenv('PATH')]),
+                'LD_LIBRARY_PATH' : os.pathsep.join(libpath),
                 }):
                 meson.configure(defs=defs,source_dir=self._source_subfolder, build_dir=self._build_subfolder,
                                 pkg_config_paths=pkg_config_paths)
